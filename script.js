41e7d6b386f5a1adb6190f66e71997904f3c5adf
Tolulope Amole <amoletolulope@gmail.com>
2022-10-05 09:14:23 +0100

    Question one solution

---
+++ script.js
@@ -0,0 +1,13 @@
+function countStep(num){
+    count = 0
+    while (num > 0){
+        if(num % 2 === 0){
+            count += 1
+            num = num/2
+        } else if (num % 2 === 1){
+            count += 1
+            num -= 1
+        } 
+    }
+    return count
+}~
